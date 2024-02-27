4f7e61186f1d72665686eb9a91848f9e46670c6d
blacktop <blacktop@users.noreply.github.com>
2024-02-26 21:40:47 -0700

    fix: codeql.sh

--- codeql.sh
+++ codeql.sh
@@ -26,7 +26,9 @@ function info() {
 }
 
 # Config
+: ${OS_TYPE:=''}
 : ${IOS_VERSION:=''}
+: ${MACOS_VERSION:=''}
 : ${WEBKIT_VERSION:=''}
 
 WORK_DIR="$PWD"
@@ -64,39 +66,68 @@ function get_version() {
     local version=$1
     while read -r tag; do
         if [[ "${tag}" == "${version}" ]]; then
-            running "✅ Exact tag: $tag"
+            echo "  ✅ Exact tag: $tag"
             WEBKIT_VERSION=$tag
             return
         # Check if the tag is less than the given version
         elif [[ "$(printf '%s\n' "$tag" "${version}" | sort -V | head -n1)" != "${version}" ]]; then
-            running "⚠️ Closest tag: $tag"
+            echo "  ⚠️ Closest tag: $tag"
             WEBKIT_VERSION=$tag
             return
         fi
-    done < <(git ls-remote --tags https://github.com/WebKit/WebKit.git WebKit-7617\* | grep -Eo 'WebKit-[0-9.]+$' | sort -Vr )
+    done < <(git ls-remote --tags https://github.com/WebKit/WebKit.git WebKit-7617\* | grep -Eo 'WebKit-[0-9.]+$' | sort -Vr)
 }
 
 function clone_webkit() {
-    if [ -z "$WEBKIT_VERSION" ] && [ -z "$IOS_VERSION" ]; then
-        gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Choose $(gum style --foreground 212 'iOS') version to build:"
-        IOS_VERSION=$(gum choose "17.3")
-        case ${IOS_VERSION} in
-        '17.3')
-            RELEASE_URL='https://raw.githubusercontent.com/apple-oss-distributions/distribution-iOS/ios-173/release.json'
+    if [ -z "$WEBKIT_VERSION" ]; then
+        if [ -z "$OS_TYPE" ]; then
+            gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Choose $(gum style --foreground 212 'OS') type:"
+            OS_TYPE=$(gum choose "macOS" "iOS")
+        fi
+        case ${OS_TYPE} in
+        'macOS')
+            if [ -z "$MACOS_VERSION" ]; then
+                gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Choose $(gum style --foreground 212 'macOS') version to build:"
+                MACOS_VERSION=$(gum choose "14.3")
+                case ${MACOS_VERSION} in
+                '14.3')
+                    RELEASE_URL='https://raw.githubusercontent.com/apple-oss-distributions/distribution-macOS/macos-143/release.json'
+                    ;;
+                *)
+                    error "Invalid macOS version"
+                    exit 1
+                    ;;
+                esac
+            fi
+            ;;
+        'iOS')
+            if [ -z "$IOS_VERSION" ]; then
+                gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Choose $(gum style --foreground 212 'iOS') version to build:"
+                IOS_VERSION=$(gum choose "17.3")
+                case ${IOS_VERSION} in
+                '17.3')
+                    RELEASE_URL='https://raw.githubusercontent.com/apple-oss-distributions/distribution-iOS/ios-173/release.json'
+                    ;;
+                *)
+                    error "Invalid iOS version"
+                    exit 1
+                    ;;
+                esac
+            fi
             ;;
         *)
-            error "Invalid iOS version"
+            error "Invalid OS type"
             exit 1
             ;;
         esac
         # Parse the latest WebKit version from the release.json and lookup in the WebKit tags
         local version
-        version=$(curl -s $RELEASE_URL | jq -r '.projects[] | select(.project=="WebKit") | .tag')
+        version=$(curl -s "$RELEASE_URL" | jq -r '.projects[] | select(.project=="WebKit") | .tag')
         info "Using version: $version"
         get_version "$version"
     fi
     if [ ! -d "${WORK_DIR}/WebKit" ]; then
-        running "⬇️ Cloning WebKit"
+        running "⬇️  Cloning WebKit"
         git clone --depth 1 --branch "${WEBKIT_VERSION}" https://github.com/WebKit/WebKit.git "${WORK_DIR}/WebKit"
     fi
 }
@@ -107,7 +138,8 @@ function create_db() {
     DATABASE_DIR="${WORK_DIR}/webkit-codeql"
     rm -rf "${DATABASE_DIR}"
     running "📦 Creating the CodeQL database..."
-    codeql database create "${DATABASE_DIR}" --language=cpp -v --command="${WEBKIT_SRC_DIR}/Tools/Scripts/build-webkit --jsc-only --debug"
+    CODEQL_CMD="./Tools/Scripts/build-webkit --jsc-only --debug"
+    codeql database create "${DATABASE_DIR}" -v --threads=0 --language=cpp --source-root="${WEBKIT_SRC_DIR}" --command="${CODEQL_CMD}"
     info "Deleting log files..."
     rm -rf "${DATABASE_DIR}"/log
     info "Zipping the CodeQL database..."
